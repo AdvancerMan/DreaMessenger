e5c91dea4483566d2ff8f1173434e63dcdcab0c4
Kazakov Mihail <advancerman@gmail.com>
2022-02-24 17:42:37 +0300

    Add authentication

--- messenger/urls.py
+++ messenger/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from messenger.views import HelloView
+from messenger.views import HelloView, LoginView, LogoutView
 
 urlpatterns = [
-    path('hello/', HelloView.as_view())
+    path('hello/', HelloView.as_view()),
+    path('login/', LoginView.as_view()),
+    path('logout/', LogoutView.as_view()),
 ]
--- messenger/views.py
+++ messenger/views.py
@@ -1,3 +1,6 @@
+import json
+
+from django.contrib.auth import authenticate, login, logout
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -5,3 +8,31 @@ from rest_framework.views import APIView
 class HelloView(APIView):
     def get(self, request):
         return Response({"message": "Hello world!"})
+
+
+class LoginView(APIView):
+    permission_classes = []
+
+    def post(self, request):
+        data = json.loads(request.body)
+        username = data.get('username')
+        password = data.get('password')
+        if username is None or password is None:
+            return Response(
+                {"detail": "Invalid credentials"},
+                status=400
+            )
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return Response({"detail": "Success"})
+        return Response(
+            {"detail": "Invalid credentials"},
+            status=400,
+        )
+
+
+class LogoutView(APIView):
+    def post(self, request):
+        logout(request)
+        return Response({"detail": "Success"})
