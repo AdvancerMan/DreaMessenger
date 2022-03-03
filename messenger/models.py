b5cb0e80384bb777c13982f7bc08dac7eacfe500
Kazakov Mihail <advancerman@gmail.com>
2022-03-03 17:49:22 +0300

    Add models

--- messenger/models.py
+++ messenger/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Dialogue(models.Model):
+    users = models.ManyToManyField(User, related_name='dialogues')
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+
+class Message(models.Model):
+    dialogue = models.ForeignKey(Dialogue, on_delete=models.CASCADE, related_name='messages')
+    from_user = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='messages', blank=True, null=True)
+    picture = models.BinaryField(max_length=1024 * 1024)
+    is_edited = models.BooleanField(default=False)
+    edited_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
